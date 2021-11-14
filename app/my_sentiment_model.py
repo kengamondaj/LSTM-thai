9fd39917e6296085be6585137b909e8e31abc966
kengamondaj <kengamondaj@gmail.com>
2021-11-15 03:54:01 +0700

    update

--- app/my_sentiment_model.py
+++ app/my_sentiment_model.py
@@ -1,6 +1,65 @@
+from pythainlp import word_tokenize
+import torch
+import json
+import re
+import emoji
+from pythainlp import word_tokenize
+import numpy as np
+
 class MySentimentModel:
     def __init__(self):
+        a_file = open("Models/vocab2index.json", "r")
+        self.model = torch.load('Models/model')
+        self.vocab2index = json.load(a_file)
         pass
+    
+    def replace_url(self,text):
+        URL_PATTERN = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))"""
+        return re.sub(URL_PATTERN, 'xxurl', text)
 
-    def predict(text: str) -> str:
-        return text
+    def replace_rep(self,text):
+        def _replace_rep(m):
+            c,cc = m.groups()
+            return f'{c}xxrep'
+        re_rep = re.compile(r'(\S)(\1{2,})')
+        return re_rep.sub(_replace_rep, text)
+
+    def ungroup_emoji(self,toks):
+        res = []
+        for tok in toks:
+            if emoji.emoji_count(tok) == len(tok):
+                for char in tok:
+                    res.append(char)
+            else:
+                res.append(tok)
+        return res
+
+    def process_text(self,text):
+        #pre rules
+        res = text.lower().strip()
+        res = self.replace_url(res)
+        res = self.replace_rep(res)
+        
+        #tokenize
+        res = [word for word in word_tokenize(res) if word and not re.search(pattern=r"\s+", string=word)]
+        
+        #post rules
+        res = self.ungroup_emoji(res)
+        
+        return res
+
+    def encode_sentence(self,text, vocab2index, N=100):
+        tokenized = text.split('|')
+        encoded = np.zeros(N, dtype=int)
+        enc1 = np.array([self.vocab2index.get(word, self.vocab2index["UNK"]) for word in tokenized])
+        length = min(N, len(enc1))
+        encoded[:length] = enc1[:length]
+        return encoded, length
+
+    def predict(self,text: str) -> str:
+        text = '|'.join(self.process_text(text))
+        test_texts ,length_test_text = self.encode_sentence(test_texts,self.vocab2index )
+        y_pred = self.model(torch.from_numpy(test_texts.reshape(1,-1)), torch.Tensor([length_test_text]))
+        y_pred = np.argmax(y_pred.detach().numpy())
+        class_map = {0:'neu', 1:'neg', 2:'pos', 3:'q'}
+        return class_map[y_pred]
